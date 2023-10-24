f2a351aa989f6c8aebfee719827fcab7cb786e67
Naveen Maanju <naveenmaanju@naveens-air.lan>
2023-10-24 18:32:32 +0200

    removed unwanted code

--- src/test/java/org/d3softtech/oauth2/server/response/JWKSResponse.java
+++
@@ -1,8 +0,0 @@
-package org.d3softtech.oauth2.server.response;
-
-public record JWKSResponse(JWKSEntry[] jwksEntries) {
-
-  public record JWKSEntry(String kid, String n, String kty, String e){
-
-  }
-}
